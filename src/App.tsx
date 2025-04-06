9ae5c5523ebb4d786f82198350927656ddb79c1b
Renan Castro <renandf@gmail.com>
2025-04-06 18:36:17 +1000

    feat: Add feature to toggle themes (dark, light, system)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { RouterProvider } from 'react-router'
 import { Toaster } from 'sonner'
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
-    <>
+    <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
       <Toaster richColors />
       <RouterProvider router={router} />
-    </>
+    </ThemeProvider>
   )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 import { Separator } from '@/components/ui/separator'
 import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
 import { NavLink } from './nav-link'
+import { ThemeToggle } from './theme/theme-toggle'
 
 export function Header() {
   return(
@@ -21,6 +22,10 @@ export function Header() {
             Orders
           </NavLink>
         </nav>
+
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+        </div>
       </div>
     </header>
   )
